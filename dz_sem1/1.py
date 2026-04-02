fb7351398b59d794af99e10f3dac66627afafe96
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-04-02 09:17:06 +0300

    new file: 1.py || Next smaller element

---
+++ dz_sem1/1.py
@@ -0,0 +1,26 @@
+def next_smaller_element(old: list([int])) -> list([int]):
+    stack = []
+    n = len(old)
+    result = [-1] * n
+
+    for i in range(n - 1, -1, -1):
+        while stack and stack[-1] >= old[i]:
+            stack.pop()
+
+        if stack:
+           result[i] = stack[-1]
+
+        stack.append(old[i])
+
+    return result
+
+def test():
+    a = [4, 5, 2, 10, 8]
+    print(f"Массив:       {a}")
+    print(f"Next smaller: {next_smaller_element(a)}")
+
+def main():
+    test()
+
+if __name__ == "__main__":
+    main()
