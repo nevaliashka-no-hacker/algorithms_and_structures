d534431eebeb478bbee70a11e5f9f0c35374e535
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-04-05 15:04:14 +0300

    second task was defeated

---
+++ lab_algos2/B4.py
@@ -0,0 +1,61 @@
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+def build_list(values):
+    """Создать список из массива: build_list([1,2,3]) → 1→2→3→None"""
+    dummy = ListNode(0)
+    cur = dummy
+    for v in values:
+        cur.next = ListNode(v)
+        cur = cur.next
+    return dummy.next
+
+def count_above_average(head) -> int:
+    '''Подсчитать, сколько элементов списка строго больше
+       среднего арифметического всех элементов:
+       1 проверка на пустой
+       2 длина, сумма, результат, ср арифм
+       3 проходимся по всему и отдельно по последнему,
+         считаем длину и сумму
+       4 находим ср арифм
+       5 проходимся по всему и отдельно по последнему,
+         считаем подходящие элементы'''
+
+    if not head:
+        return 0
+
+    len_head = 0
+    sum_head = 0
+    count_el = 0
+    arith_mean = 0
+
+    cur = head
+    while cur.next:
+        len_head += 1
+        sum_head += cur.val
+        cur = cur.next
+    len_head += 1
+    sum_head += cur.val
+    arith_mean = sum_head // len_head
+
+    cur = head
+    while cur.next:
+        if cur.val > arith_mean:
+            count_el += 1
+        cur = cur.next
+
+    if cur.val > arith_mean:
+            count_el += 1
+
+    print(len_head, sum_head, arith_mean)
+
+    return count_el
+
+def main():
+    head = build_list([1, 2, 3, 4, 10])
+    print(count_above_average(head))
+
+if __name__ == "__main__":
+    main()
