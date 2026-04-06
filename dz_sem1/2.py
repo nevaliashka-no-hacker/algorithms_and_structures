7fde5c7263f16c4dfb6edd810ad8eb1d312d71ff
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-04-06 14:58:29 +0300

    comments and tasks

---
+++ dz_sem1/2.py
@@ -0,0 +1,34 @@
+'''
+Используем стек, в который сохраняем индексы 
+повторяющихся элементов слева направо.
+Далее в обратном порядке кладем в результирующую строку.
+
+Вход:  "abbaca"
+Выход: "ca"
+Пояснение: "abbaca" → "aaca" → "ca"
+'''
+
+def pop_duplicate(old_str: str) -> str:
+    n = len(old_str)
+    new_str = str()
+    stack = []
+
+    for i in range(n):
+        if stack and old_str[i] == old_str[stack[-1]]:
+            stack.pop()
+            continue
+
+        stack.append(i)
+
+    while stack:
+        new_str = str(old_str[stack[-1]]) + new_str
+        stack.pop()
+    return new_str
+
+def main():
+    s = "abbaca"
+    result = pop_duplicate(s)
+    print(result)
+
+if __name__ == '__main__':
+    main()
