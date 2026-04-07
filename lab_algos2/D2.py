6229e769746f8f9cf7239b47243fcda0d3621d3e
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-04-07 13:45:11 +0300

    fourth task was finally completed, but not without the help of the AI(((( not written off, of course

--- lab_algos2/D2.py
+++ lab_algos2/D2.py
@@ -19,24 +19,50 @@ def get_intersection(headA, headB) -> int | None:
     A:     1 → 2 ↘
                     5 → 6 → None
     B: 3 → 4 → 7 ↗
-    Ответ: узел со значением 5 '''
+    Ответ: узел со значением 5
+
+    1 проверка на пустой
+    2 идем в концы списков, измеряем длину
+    3 длина не совпадает -> несколько первых элементов
+      большего списка нет в меньшем -> проходим эти элементы,
+      не берем их в расчет
+    4 одновременно проходимся по спискам,
+      сравнивая узлы
+    5 равны -> искомый узел, т.к. значение и адрес следующего
+      будут равны'''
 
     if not headA or not headB:
         return None
-    
+
     curA = headA
+    lenA = 0
     curB = headB
-    while curA:
-        while curB:
-            if curA.val == curB.val:
-                if curA.next and curB and curA.next == curB:
-                    return curA.val
-            curB = curB.next
+    lenB = 0
+
+    while curA.next:
+        lenA += 1
         curA = curA.next
 
-    if curA and curB:
-        if curA.val == curB.val:
-            return curA.val
+    while curB.next:
+        lenB += 1
+        curB = curB.next
+
+    curA = headA
+    curB = headB
+    while lenA > lenB:
+        lenA -= 1
+        curA = cur.next
+
+    while lenB > lenA:
+        lenB -= 1
+        curB = curB.next
+
+    while curA.next and curB.next:
+        if curA == curB:
+            return curA
+        curA = curA.next
+        curB = curB.next
+        
     
     return None
 
@@ -44,7 +70,7 @@ def get_intersection(headA, headB) -> int | None:
 def main():
     common = build_list([5, 6])
     headA = ListNode(1, ListNode(2, common))
-    headB = ListNode(3, ListNode(4, ListNode(7, common)))
+    headB = ListNode(3, ListNode(6, ListNode(7, common)))
     node = get_intersection(headA, headB)
     print(node.val if node else None)  # 5
 
